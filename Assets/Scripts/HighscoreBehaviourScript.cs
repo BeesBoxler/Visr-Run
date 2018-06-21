d1c1d85ef2fbeb9e47e5bcd95f0e5e706b9aff04
yiming.zhou <yiming.zhou@3f8c0caa-311f-e941-ad8e-23d4b310951f>
2018-06-21 11:32:15 +0000

    git-svn-id: https://svn.visr-vr.com/GameJamRunner@5 3f8c0caa-311f-e941-ad8e-23d4b310951f

---
+++ Assets/Scripts/HighscoreBehaviourScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class HighscoreBehaviourScript : MonoBehaviour {
+    // Use this for initialization
+    public Text scoreText;
+    public Text listboard;
+    List<Record> record;
+    void Start () {
+
+
+
+        int score = StaticScoreClass.ScoreInformation;
+        Debug.Log("your score is "+score);
+        record = new List<Record>();
+
+        scoreText = GetComponent("scoreText") as Text;
+        scoreText.text = score.ToString();
+        listboard = GetComponent("listText") as Text;
+
+
+
+        string path = "Assets/record.txt";
+        if (!System.IO.File.Exists(path))
+        {
+            Debug.Log("not found record.txt, create new file");
+            StreamWriter sw = new StreamWriter(path, true);
+            sw.WriteLine("test1|100000,");
+            sw.WriteLine("test2|200000,");
+            sw.WriteLine("test3|300000,");
+            sw.WriteLine("test4|400000,");
+            sw.WriteLine("test5|500000,");
+            sw.WriteLine("test6|600000,");
+            sw.Close();
+
+        }
+
+            StreamReader sr = new StreamReader(path);
+            string allline =sr.ReadToEnd();
+            string[] lines = allline.Split(',');
+            foreach(string line in lines)
+            {
+            if (line.Length>2)
+            {
+                string[] thisline = line.Split('|');
+                record.Add(new Record(thisline[0], Int32.Parse(thisline[1])));
+            }
+            }
+            sr.Close();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void print()
+    {
+        foreach(Record r in record)
+        {
+            Debug.Log(r.ToString());
+        }
+    }
+
+
+}
+public class Record
+{
+    public string name;
+    public int score;
+    public Record(string iname,int iscore)
+    {
+        name = iname;
+        score = iscore;
+    }
+    public override string ToString()
+    {
+        return name + "   " + score.ToString();
+    }
+
+}
